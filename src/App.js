2b134b7ee8edf574a9aaa627f139710cc4605d96
Thinh Truong <truongthinh619@gmail.com>
2020-09-04 14:26:40 +0000

    Cookie test

--- src/App.js
+++ src/App.js
@@ -6,6 +6,24 @@ import {excuName} from './logon'
   var items;
   var itemsss = [];
 
+  function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)===' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+  var userName123 = getCookie("userName");
+  // eslint-disable-next-line
+  console.log('userName123');
 
   class MyToDoList extends React.Component {
     constructor(props) {
--- src/logon.js
+++ src/logon.js
@@ -2,8 +2,14 @@ import React from 'react' ;
 import firebase from 'firebase';
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import Register from './login'
-import MyToDoList from './app';
+import MyToDoList from './App';
 
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
 
 var name = [];
 var password = '';
@@ -82,6 +88,7 @@ class Logon extends React.Component {
 
     render(){
         if(this.state.isData === true) {
+            setCookie("userName", excuName, 1);
             return(
                 <div>
                     <BrowserRouter>
